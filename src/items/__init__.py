dc0c9850334ba048e63de10b0631cf1d21983b0c
andres0772 <andresesteb0772@gmail.com>
2026-04-12 13:22:43 -0500

    feat: actualizar __init__.py de items con exports

--- src/items/__init__.py
+++ src/items/__init__.py
@@ -1,3 +1,7 @@
-"""Items del juego.
-Clases para tesoro, equipamiento y otros items recolectables.
-"""
+"""Módulo de items del juego."""
+
+from .item import Item, Consumible, Tesoro
+from .trampa import TrampaExplosiva
+from .equipamiento import Equipamiento
+
+__all__ = ["Item", "Consumible", "Tesoro", "TrampaExplosiva", "Equipamiento"]
